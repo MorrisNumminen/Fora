287cd3454b8851208bc9f2d567f57a6cea8ceba6
GoosePerfect <efraim.kirkhorn@gmail.com>
2022-05-29 22:17:31 +0200

    Update Search

---
+++ Fora/Shared/SearchDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared
+{
+    public class SearchDto
+    {
+        public string SearchWord { get; set; }
+        public string SearchMethod { get; set; }
+        public string SortMethod { get; set; }
+    }
+}
